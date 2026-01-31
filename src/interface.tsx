b0056ed50a3db5f63b02feabd2ecd5e5eb332e14
Andriantsitohaina <andriantsitohaina.luca@gmail.com>
2026-02-01 01:14:14 +0300

    fixed the interface

--- src/interface.tsx
+++ src/interface.tsx
@@ -1,4 +1,4 @@
-import type { Airport, FlightFilters, FlightItinerary } from "./flight.types";
+
 
 export interface FlightSearchParams {
   originSkyId: string;
@@ -16,32 +16,4 @@ export interface FlightSearchParams {
 }
 
 //store interface for zustand
-export interface FlightStore {
-  // Search params
-  origin: Airport | null;
-  destination: Airport | null;
-  departureDate: string;
-  returnDate: string;
-  passengers: number;
-  
-  // Results
-  flights: FlightItinerary[];
-  filteredFlights: FlightItinerary[];
-  loading: boolean;
-  error: string | null;
-  
-  // Filters
-  filters: FlightFilters;
-  
-  // Actions
-  setOrigin: (airport: Airport | null) => void;
-  setDestination: (airport: Airport | null) => void;
-  setDepartureDate: (date: string) => void;
-  setReturnDate: (date: string) => void;
-  setPassengers: (count: number) => void;
-  setFlights: (flights: FlightItinerary[]) => void;
-  setLoading: (loading: boolean) => void;
-  setError: (error: string | null) => void;
-  setFilters: (filters: Partial<FlightFilters>) => void;
-  applyFilters: () => void;
-}
+
